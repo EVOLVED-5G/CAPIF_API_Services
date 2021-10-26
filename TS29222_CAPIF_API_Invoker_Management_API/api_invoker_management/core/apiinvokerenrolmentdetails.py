eedd095ea73f64d8ea249813590f2e2d43574b39
Stavros Charismiadis <stauros411@gmail.com>
2021-10-26 14:30:46 +0300

    Change 202 error to 403, when invoker tries to register twice, Set public key as unique for each invoker

--- TS29222_CAPIF_API_Invoker_Management_API/api_invoker_management/core/apiinvokerenrolmentdetails.py
+++ TS29222_CAPIF_API_Invoker_Management_API/api_invoker_management/core/apiinvokerenrolmentdetails.py
@@ -3,6 +3,7 @@ import secrets
 from flask import current_app, Flask, Response
 import json
 from ..encoder import JSONEncoder
+from ..models.problem_details import ProblemDetails
 
 
 def add_apiinvokerenrolmentdetail(apiinvokerenrolmentdetail):
@@ -20,19 +21,17 @@ def add_apiinvokerenrolmentdetail(apiinvokerenrolmentdetail):
     mydb = myclient[db]
     mycol = mydb[col]
 
-    res = mycol.find({'$or': [{'onboarding_information.api_invoker_certificate': apiinvokerenrolmentdetail.onboarding_information.api_invoker_certificate},
-                                    {'onboarding_information.api_invoker_public_key': apiinvokerenrolmentdetail.onboarding_information.api_invoker_public_key},
-                                    {'onboarding_information.onboarding_secret': apiinvokerenrolmentdetail.onboarding_information.onboarding_secret},
-                                    {'notification_destination': apiinvokerenrolmentdetail.notification_destination}
-                                    ]})
+    res = mycol.find({'onboarding_information.api_invoker_public_key': apiinvokerenrolmentdetail.onboarding_information.api_invoker_public_key})
+
     if res.count() != 0:
         myclient.close()
-        return Response("The CAPIF core has accepted the Onboarding request and is processing it.", status=202, mimetype='application/json')
+        prob = ProblemDetails(title="Forbidden", status=403, detail="Invoker already registered", cause="Identical invoker public key")
+        return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype='application/json')
     else:
         apiinvokerenrolmentdetail.api_invoker_id = secrets.token_hex(15)
-        mycol.insert_one(apiinvokerenrolmentdetail.to_dict())
+        _id = mycol.insert_one(apiinvokerenrolmentdetail.to_dict())
         myclient.close()
-        res = Response(json.dumps(apiinvokerenrolmentdetail, cls=JSONEncoder), status=200, mimetype='application/json')
-        res.headers['Location'] = "http://localhost:8080/api-invoker-management/v1/onboardedInvokers/" + apiinvokerenrolmentdetail.api_invoker_id
+        res = Response(json.dumps(apiinvokerenrolmentdetail, cls=JSONEncoder), status=201, mimetype='application/json')
+        res.headers['Location'] = "http://localhost:8080/api-invoker-management/v1/onboardedInvokers/" + str(_id.inserted_id)
         return res
 
