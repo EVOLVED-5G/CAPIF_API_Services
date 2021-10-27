e7dd101e69c098e64e3f84392e64943eccf82016
Alejandro Molina <alemosan@hotmail.com>
2021-10-27 10:35:39 +0200

    jwt added to update and delete method

--- TS29222_CAPIF_API_Invoker_Management_API/api_invoker_management/controllers/default_controller.py
+++ TS29222_CAPIF_API_Invoker_Management_API/api_invoker_management/controllers/default_controller.py
@@ -10,7 +10,7 @@ import secrets
 import json
 from flask_jwt_extended import jwt_required
 
-
+@jwt_required()
 def onboarded_invokers_onboarding_id_delete(onboarding_id):  # noqa: E501
     """onboarded_invokers_onboarding_id_delete
 
@@ -25,7 +25,7 @@ def onboarded_invokers_onboarding_id_delete(onboarding_id):  # noqa: E501
    
     return  apiinvokerenrolmentdetails.remove_apiinvokerenrolmentdetail(onboarding_id)
 
-
+@jwt_required()
 def onboarded_invokers_onboarding_id_put(onboarding_id, body):  # noqa: E501
     """onboarded_invokers_onboarding_id_put
 
