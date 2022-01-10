956b0bd9b0b60b2aa4794395abc1d0d99e4d4fe0
Jorge Moratinos <jorge.moratinossalcines@telefonica.com>
2022-01-10 16:25:09 +0100

    Added some comments to scripts (#24)

--- clean_capif_docker_services.sh
+++ clean_capif_docker_services.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+docker-compose down --rmi all --remove-orphans
+status=$?
+if [ $status -eq 0 ]; then
+    echo "*** All Capif services are cleaned ***"
+else
+    echo "*** Some Capif services failed on clean ***"
+fi
 
-docker-compose down --rmi all --remove-orphans || true
+exit $status
+
