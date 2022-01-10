6618fd1631fe4cbb864c5755a8cfd6785164b0b9
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2022-01-10 16:15:52 +0100

    Added some comments to scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 docker-compose up --detach --remove-orphans --build
+status=$?
+if [ $status -eq 0 ]; then
+    echo "*** All Capif services are running ***"
+else
+    echo "*** Some Capif services failed to start ***"
+fi
 
-
-
+exit $status
