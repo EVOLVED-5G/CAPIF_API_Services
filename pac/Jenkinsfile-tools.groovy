6b19aa8e4b163750b5c6d245030bbda907ddc33a
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2022-01-12 11:46:16 +0100

    Added BRANCH_NAME to tools generation

--- pac/Jenkinsfile-tools.groovy
+++ pac/Jenkinsfile-tools.groovy
@@ -13,11 +13,13 @@ pipeline {
         ansiColor('xterm')
     }
     parameters {
+        string(name: 'BRANCH_NAME', defaultValue: 'develop', description: 'Deployment git branch name')
         choice(name: 'FORCE_DOCKER_CLEAN_BUILD', choices: ['False', 'True'], description: 'Force Docker Clean Build. Default use cached images (False)')
         string(name: 'ROBOT_DOCKER_IMAGE_VERSION', defaultValue: '2.0', description: 'Robot Docker image version')
         booleanParam(name: 'GENERATE_ROBOT_DOCKER_IMAGE', defaultValue: false, description: 'Check if robot docker image should be generated')
     }
     environment {
+        BRANCH_NAME = "${params.BRANCH_NAME}"
         CACHE = forceDockerCleanBuild("${params.FORCE_DOCKER_CLEAN_BUILD}")
         ROBOT_VERSION = dockerVersion("${params.ROBOT_DOCKER_IMAGE_VERSION}")
         GENERATE_ROBOT = "${params.GENERATE_ROBOT_DOCKER_IMAGE}"
