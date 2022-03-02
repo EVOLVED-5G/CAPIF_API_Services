8b27c371926b4b194dc9e043bc32d8fe29cb109f
Alejandro Molina <alemosan@hotmail.com>
2022-03-02 11:41:52 +0100

    feat: added bucket

--- iac/terraform/openshift4/backend.tf
+++ iac/terraform/openshift4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt        = true
-    bucket         = "evolved5g-capif-openshift4-terraform-states"
+    bucket         = "evolved5g-capif-athens-terraform-states"
     key            = "capif"
     region         = "eu-central-1"
     dynamodb_table = "terraform_locks"
