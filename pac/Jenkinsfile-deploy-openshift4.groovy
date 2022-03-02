4c22a7444df9718110e0512e47610bbd27b58c58
Alejandro Molina <alemosan@hotmail.com>
2022-03-02 11:15:31 +0100

    tterraform

--- pac/Jenkinsfile-deploy-openshift4.groovy
+++ pac/Jenkinsfile-deploy-openshift4.groovy
@@ -23,12 +23,10 @@ pipeline {
     stages {
         stage('Login openshift') {
             steps {
-                withCredentials([string(credentialsId: '7e6e1098-f471-4371-b3ef-9787fb2248a1', variable: 'TOKEN')]) {
                     dir ("${env.WORKSPACE}/iac/terraform/openshift4") {
                         sh '''
                             cp kubeconfigAthens ~/kubeconfig
                             export KUBECONFIG="./kubeconfig"
-                            oc login --insecure-skip-tls-verify --token=$TOKEN $OPENSHIFT_URL
                         '''
                         readFile('kubeconfigAthens')
                     }
