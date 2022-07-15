cbc2bfff01b54a7aa87013e6de3a6004e708a0cc
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2022-07-15 11:48:54 +0200

    Provider updated

--- iac/terraform/openshift4/provider.tf
+++ iac/terraform/openshift4/provider.tf
@@ -1,5 +1,10 @@
+# provider "kubernetes" {
+# config_path = "~/kubeconfig"
+# config_context = "evol5-capif/api-ocp-epg-hi-inet:6443/system:serviceaccount:evol5-capif:deployer"
+# insecure = true
+# }
+
 provider "kubernetes" {
-config_path = "~/kubeconfig"
-config_context = "evol5-capif/api-ocp-epg-hi-inet:6443/system:serviceaccount:evol5-capif:deployer"
-insecure = true
+  config_path    = "~/kubeconfig"
+  insecure       = true
 }
