06d0314e16ed7dbfea581fd886c7bcf1766ed04f
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-07-15 14:46:21 +0200

    style: changes 2

--- iac/terraform/openshift4/provider.tf
+++ iac/terraform/openshift4/provider.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
   config_path    = "~/kubeconfig"
-  config_context = "evol5-capif/api-ocp-epg-hi-inet:6443/system:serviceaccount:evol5-capif:deployer"
+  config_context = "deployer"
   insecure       = true
 }
