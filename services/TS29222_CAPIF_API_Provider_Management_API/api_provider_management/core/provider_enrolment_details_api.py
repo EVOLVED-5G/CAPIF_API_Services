d82cd050ddd58e700d89c9757d655c2479c1725f
nacho <hjoseigancio@gmail.com>
2022-08-26 13:43:05 +0200

    fix put/patch errors

--- services/TS29222_CAPIF_API_Provider_Management_API/api_provider_management/core/provider_enrolment_details_api.py
+++ services/TS29222_CAPIF_API_Provider_Management_API/api_provider_management/core/provider_enrolment_details_api.py
@@ -55,10 +55,33 @@ class ProviderManagementOperations:
                 return Response(json.dumps(provider_enrolment_details, default=str, cls=JSONEncoder), status=204, mimetype='application/json')
 
         except Exception as e:
-            print("An exception occurred ::", e)
+            print("An exception occurred ::", e, file=sys.stderr)
             return False
 
-    def update_api_provider_enrolment_details(self, api_provider_enrolment_details, registrationId):
+    def update_api_provider_enrolment_details(self, registrationId, api_provider_enrolment_details):
+        try:
+            mycol = self.db.get_col_by_name(self.db.provider_enrolment_details)
+            search_filter = {'registration_id': registrationId}
+            old_provider_enrolment_details = mycol.find_one(search_filter)
+
+            if  old_provider_enrolment_details is None:
+                prob = ProblemDetails(title="Not Found", status=404, detail="Not Exist Provider Enrolment Details",
+                                    cause="Not found registrations to send this api provider details")
+                return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype='application/json')
+            else:
+                api_provider_enrolment_details = api_provider_enrolment_details.to_dict()
+                api_provider_enrolment_details = {
+                    key: value for key, value in api_provider_enrolment_details.items() if value is not None
+                }
+
+                mycol.update_one(old_provider_enrolment_details, {"$set":api_provider_enrolment_details}, upsert=False)
+                return  Response(json_util.dumps(api_provider_enrolment_details, cls=JSONEncoder), status=200, mimetype='application/json')
+
+        except Exception as e:
+            print("An exception occurred ::", e, file=sys.stderr)
+            return False
+
+    def patch_api_provider_enrolment_details(self, registrationId, api_provider_enrolment_details_patch):
         try:
             mycol = self.db.get_col_by_name(self.db.provider_enrolment_details)
 
@@ -72,31 +95,16 @@ class ProviderManagementOperations:
                 return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype='application/json')
 
             else:
-                result = mycol.update_one(old_provider_enrolment_details, api_provider_enrolment_details.to_dict())
-                return  Response(json.dumps(result, cls=JSONEncoder), status=200, mimetype='application/json')
+
+                api_provider_enrolment_details_patch = api_provider_enrolment_details_patch.to_dict()
+                api_provider_enrolment_details_patch = {
+                    key: value for key, value in api_provider_enrolment_details_patch.items() if value is not None
+                }
+
+                mycol.update_one(old_provider_enrolment_details, {"$set":api_provider_enrolment_details_patch})
+
+                return  Response(json_util.dumps(api_provider_enrolment_details_patch, cls=JSONEncoder), status=200, mimetype='application/json')
 
         except Exception as e:
-            print("An exception occurred ::", e)
-            return False
-
-    def patch_api_provider_enrolment_details(self, api_provider_enrolment_details_patch, registrationId):
-        try:
-            mycol = self.db.get_col_by_name(self.db.provider_enrolment_details)
-
-            search_filter = {'registration_id': registrationId}
-            old_provider_enrolment_details = mycol.find_one(search_filter)
-
-
-            if  old_provider_enrolment_details is None:
-                prob = ProblemDetails(title="Not Found", status=404, detail="Not Exist Provider Enrolment Details",
-                                    cause="Not found registrations to send this api provider details")
-                return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype='application/json')
-
-            else:
-                result = mycol.update_one(old_provider_enrolment_details, api_provider_enrolment_details_patch.to_dict())
-
-                return  Response(json.dumps(result, cls=JSONEncoder), status=200, mimetype='application/json')
-
-        except Exception as e:
-            print("An exception occurred ::", e)
+            print("An exception occurred ::", e, file=sys.stderr)
             return False
