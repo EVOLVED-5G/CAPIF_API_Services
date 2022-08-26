61bf035b99494168a4c1d7ab159a9d27ffb76c6e
raxonros <hjoseigancio@gmail.com>
2022-08-26 14:19:42 +0200

    Capif provider management api (#49)
    * add provider api manamgement core operations
    
    * add operations to controller
    
    * fix errors
    
    * add Test plan api provider management
    
    * fix errors
    
    * fix put/patch errors
    
    * remove white lines

--- docs/test_plan/api_provider_management/test_plan.md
+++ docs/test_plan/api_provider_management/test_plan.md
@@ -2,14 +2,168 @@
 
 - [Test Plan for CAPIF Api Provider Management](#test-plan-for-capif-api-provider-management)
 - [Tests](#tests)
-
+  - [Test Case 1: Register Api Provider](#test-case-1-register-api-provider)
+  - [Test Case 2: Register Api Provider Already registered](#test-case-2-register-api-provider-already-registered)
+  - [Test Case 3: Update Registered Api Provider](#test-case-3-update-registered-api-provider)
+  - [Test Case 4: Update Not Registered Api Provider](#test-case-4-update-not-registered-api-provider)
+  - [Test Case 5: Partially Update Registered Api Provider](#test-case-5-update-not-registered-api-provider)
+  - [Test Case 6: Partially Update Not Registered Api Provider](#test-case-6-update-not-registered-api-provider)
+  - [Test Case 7: Delete Registered Api Provider](#test-case-7-delete-registered-api-provider)
+  - [Test Case 8: Delete Not Registered Api Provider](#test-case-8-delete-not-registered-api-provider)
 
 
 # Test Plan for CAPIF Api Provider Management
 At this documentation you will have all information and related files and examples of test plan for this API.
 
 # Tests
-NO CURRENTLY IMPLEMENTED
 
+## Test Case 1: Register Api Provider
+  
+  This test case will check that Api Provider can be registered 
+
+* Pre-Conditions:
+  
+  Api Provider was not registered previously.
+
+* Actions:
+
+  Register Api Provider
+  
+  Request Body: [request body]
+
+* Post-Conditions:
+  
+  201 API invoker on-boarded successfully.
+
+  Header Location at response contains the URI of the newly created resource, according to the structure: {apiRoot}/api-provider-management/v1/onboardedInvokers/{registerId}
+
+
+## Test Case 2: Register Api Provider Already registered
+  
+  This test case will check that a Api Provider previously registered canot be re-registered
+
+* Pre-Conditions:
+  
+  Api Provider was registered previously and there is a {registerId} for his Api Provider in the DB
+
+* Actions:
+
+  Register Api Provider
+  
+  Request Body: [request body]
+
+* Post-Conditions:
+  
+  403 Forbidden returned.
+
+## Test Case 3: Update Registered Api Provider  
+  
+  This test case will check that a Registered Api Provider can be updated  
+
+* Pre-Conditions:
+  
+  Api Provider was registered previously and there is a {registerId} for his Api Provider in the DB
+
+* Actions:
+
+  Update Api Provider onboardingDetails
+  
+  Request Body: [request body]
+
+* Post-Conditions:
+  
+  200 API invoker details updated successfully.
+
+## Test Case 4: Update Not Registered Api Provider 
+  
+  This test case will check that a Non-Registered Api Provider cannot be updated  
+
+* Pre-Conditions:
+  
+  Api Provider was not registered previously.
+
+* Actions:
+
+  Update Api Provider onboardingDetails
+  
+  Request Body: [request body]
+
+* Post-Conditions:
+  
+  404 Not found.
+
+## Test Case 5: Partially Update Registered Api Provider  
+  
+  This test case will check that a Registered Api Provider can be partially updated  
+
+* Pre-Conditions:
+  
+  Api Provider was registered previously and there is a {registerId} for his Api Provider in the DB
+
+* Actions:
+
+  Update Api Provider onboardingDetails
+  
+  Request Body: [request body]
+
+* Post-Conditions:
+  
+  200 API invoker details updated successfully.
+
+## Test Case 6: Partially Update Not Registered Api Provider 
+  
+  This test case will check that a Non-Registered Api Provider cannot be partially updated  
+
+* Pre-Conditions:
+  
+  Api Provider was not registered previously.
+
+* Actions:
+
+  Update Api Provider onboardingDetails
+  
+  Request Body: [request body]
+
+* Post-Conditions:
+  
+  404 Not found.
+
+## Test Case 7: Delete Registered Api Provider   
+  
+  This test case will check that a Registered Api Provider can be deleted  
+
+* Pre-Conditions:
+  
+  Api Provider was registered previously.
+
+* Actions:
+
+  Delete Api Provider 
+
+
+* Post-Conditions:
+
+  204 The individual API Invoker matching registerId was offboarded.
+
+## Test Case 8: Delete Not Registered Api Provider
+
+  This test case will check that a Non-Registered Api Provider cannot be deleted
+
+* Pre-Conditions:
+
+  Api Provider was not registered previously.
+
+* Actions:
+
+  Delete Api Provider
+
+
+* Post-Conditions:
+
+  404 Not Found.
+
+
+
+[request body]: ./provider_details_post_example.json  "API Invoker Request"
 
 [Return To All Test Plans]: ../README.md
---
+++ services/TS29222_CAPIF_API_Provider_Management_API/api_provider_management/core/provider_enrolment_details_api.py
@@ -0,0 +1,107 @@
+import sys
+
+import pymongo
+import secrets
+from flask import current_app, Flask, Response
+import json
+from ..encoder import JSONEncoder
+from ..models.problem_details import ProblemDetails
+from bson import json_util
+from ..db.db import MongoDatabse
+
+class ProviderManagementOperations:
+
+    def __init__(self):
+        self.db = MongoDatabse()
+
+    def register_api_provider_enrolment_details(self, api_provider_enrolment_details):
+        try:
+            mycol = self.db.get_col_by_name(self.db.provider_enrolment_details)
+
+            apiProvDomId = secrets.token_hex(15)
+            registrationId = secrets.token_hex(15)
+            provider_enrolment_details = dict()
+            provider_enrolment_details["registration_id"] = registrationId
+            api_provider_enrolment_details.api_prov_dom_id = apiProvDomId
+
+            provider_enrolment_details.update(api_provider_enrolment_details.to_dict())
+            mycol.insert_one(provider_enrolment_details)
+
+            res = Response(json_util.dumps(api_provider_enrolment_details, cls=JSONEncoder),
+                        status=201, mimetype='application/json')
+            res.headers['Location'] = "http://localhost:8080/api-provider-management/v1/registrations/" + str(registrationId)
+            return res
+
+        except Exception as e:
+            print("An exception occurred ::", e, file=sys.stderr)
+            return False
+
+    def delete_api_provider_enrolment_details(self, registrationId):
+        try:
+            mycol = self.db.get_col_by_name(self.db.provider_enrolment_details)
+
+            search_filter = {'registration_id': registrationId}
+            provider_enrolment_details = mycol.find_one(search_filter)
+
+            if provider_enrolment_details is None:
+                prob = ProblemDetails(title="Not Found", status=404, detail="Not Exist Provider Enrolment Details",
+                                    cause="Not found registrations to send this api provider details")
+                return Response(json.dumps(prob, cls=JSONEncoder), status=404, mimetype='application/json')
+            else:
+                mycol.delete_one(search_filter)
+                return Response(json.dumps(provider_enrolment_details, default=str, cls=JSONEncoder), status=204, mimetype='application/json')
+
+        except Exception as e:
+            print("An exception occurred ::", e, file=sys.stderr)
+            return False
+
+    def update_api_provider_enrolment_details(self, registrationId, api_provider_enrolment_details):
+        try:
+            mycol = self.db.get_col_by_name(self.db.provider_enrolment_details)
+            search_filter = {'registration_id': registrationId}
+            old_provider_enrolment_details = mycol.find_one(search_filter)
+
+            if  old_provider_enrolment_details is None:
+                prob = ProblemDetails(title="Not Found", status=404, detail="Not Exist Provider Enrolment Details",
+                                    cause="Not found registrations to send this api provider details")
+                return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype='application/json')
+            else:
+                api_provider_enrolment_details = api_provider_enrolment_details.to_dict()
+                api_provider_enrolment_details = {
+                    key: value for key, value in api_provider_enrolment_details.items() if value is not None
+                }
+
+                mycol.update_one(old_provider_enrolment_details, {"$set":api_provider_enrolment_details}, upsert=False)
+                return  Response(json_util.dumps(api_provider_enrolment_details, cls=JSONEncoder), status=200, mimetype='application/json')
+
+        except Exception as e:
+            print("An exception occurred ::", e, file=sys.stderr)
+            return False
+
+    def patch_api_provider_enrolment_details(self, registrationId, api_provider_enrolment_details_patch):
+        try:
+            mycol = self.db.get_col_by_name(self.db.provider_enrolment_details)
+
+            search_filter = {'registration_id': registrationId}
+            old_provider_enrolment_details = mycol.find_one(search_filter)
+
+
+            if  old_provider_enrolment_details is None:
+                prob = ProblemDetails(title="Not Found", status=404, detail="Not Exist Provider Enrolment Details",
+                                    cause="Not found registrations to send this api provider details")
+                return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype='application/json')
+
+            else:
+
+                api_provider_enrolment_details_patch = api_provider_enrolment_details_patch.to_dict()
+                api_provider_enrolment_details_patch = {
+                    key: value for key, value in api_provider_enrolment_details_patch.items() if value is not None
+                }
+
+                mycol.update_one(old_provider_enrolment_details, {"$set":api_provider_enrolment_details_patch})
+
+                return  Response(json_util.dumps(api_provider_enrolment_details_patch, cls=JSONEncoder), status=200, mimetype='application/json')
+
+        except Exception as e:
+            print("An exception occurred ::", e, file=sys.stderr)
+            return False
