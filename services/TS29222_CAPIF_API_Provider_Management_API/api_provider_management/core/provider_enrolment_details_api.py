405be1fe7c1f1799e931e602c5c034ce57713e0e
Alejandro Molina Sanchez <alemosan@hotmail.com>
2022-09-01 14:11:03 +0200

    Capif code review (#52)
    * fix code errors in api invoker management and api events
    
    * fix code errors in publish api and discover api
    
    * rename .yml to .yaml
    
    * fix config syntax error
    
    * Fix minor issue of configuration file
    
    * fix code smells reported by sonarqube
    
    Co-authored-by: nacho <hjoseigancio@gmail.com>
    Co-authored-by: Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>

--- services/TS29222_CAPIF_API_Provider_Management_API/api_provider_management/core/provider_enrolment_details_api.py
+++ services/TS29222_CAPIF_API_Provider_Management_API/api_provider_management/core/provider_enrolment_details_api.py
@@ -13,6 +13,7 @@ class ProviderManagementOperations:
 
     def __init__(self):
         self.db = MongoDatabse()
+        self.mimetype = 'application/json'
 
     def register_api_provider_enrolment_details(self, api_provider_enrolment_details):
         try:
@@ -28,13 +29,13 @@ class ProviderManagementOperations:
             mycol.insert_one(provider_enrolment_details)
 
             res = Response(json_util.dumps(api_provider_enrolment_details, cls=JSONEncoder),
-                        status=201, mimetype='application/json')
-            res.headers['Location'] = "http://localhost:8080/api-provider-management/v1/registrations/" + str(registrationId)
+                        status=201, mimetype= self.mimetype)
+            res.headers['Location'] = "/api-provider-management/v1/registrations/" + str(registrationId)
             return res
 
         except Exception as e:
-            print("An exception occurred ::", e, file=sys.stderr)
-            return False
+            exception = "An exception occurred in register provider::", e
+            return Response(json.dumps(exception, default=str, cls=JSONEncoder), status=500, mimetype=self.mimetype)
 
     def delete_api_provider_enrolment_details(self, registrationId):
         try:
@@ -46,14 +47,14 @@ class ProviderManagementOperations:
             if provider_enrolment_details is None:
                 prob = ProblemDetails(title="Not Found", status=404, detail="Not Exist Provider Enrolment Details",
                                     cause="Not found registrations to send this api provider details")
-                return Response(json.dumps(prob, cls=JSONEncoder), status=404, mimetype='application/json')
+                return Response(json.dumps(prob, cls=JSONEncoder), status=404, mimetype= self.mimetype)
             else:
                 mycol.delete_one(search_filter)
-                return Response(json.dumps(provider_enrolment_details, default=str, cls=JSONEncoder), status=204, mimetype='application/json')
+                return Response(json.dumps(provider_enrolment_details, default=str, cls=JSONEncoder), status=204, mimetype= self.mimetype)
 
         except Exception as e:
-            print("An exception occurred ::", e, file=sys.stderr)
-            return False
+            exception = "An exception occurred in delete provider::", e
+            return Response(json.dumps(exception, default=str, cls=JSONEncoder), status=500, mimetype=self.mimetype)
 
     def update_api_provider_enrolment_details(self, registrationId, api_provider_enrolment_details):
         try:
@@ -64,7 +65,7 @@ class ProviderManagementOperations:
             if  old_provider_enrolment_details is None:
                 prob = ProblemDetails(title="Not Found", status=404, detail="Not Exist Provider Enrolment Details",
                                     cause="Not found registrations to send this api provider details")
-                return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype='application/json')
+                return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype= self.mimetype)
             else:
                 api_provider_enrolment_details = api_provider_enrolment_details.to_dict()
                 api_provider_enrolment_details = {
@@ -72,11 +73,11 @@ class ProviderManagementOperations:
                 }
 
                 mycol.update_one(old_provider_enrolment_details, {"$set":api_provider_enrolment_details}, upsert=False)
-                return  Response(json_util.dumps(api_provider_enrolment_details, cls=JSONEncoder), status=200, mimetype='application/json')
+                return  Response(json_util.dumps(api_provider_enrolment_details, cls=JSONEncoder), status=200, mimetype= self.mimetype)
 
         except Exception as e:
-            print("An exception occurred ::", e, file=sys.stderr)
-            return False
+            exception = "An exception occurred in update provider::", e
+            return Response(json.dumps(exception, default=str, cls=JSONEncoder), status=500, mimetype=self.mimetype)
 
     def patch_api_provider_enrolment_details(self, registrationId, api_provider_enrolment_details_patch):
         try:
@@ -89,7 +90,7 @@ class ProviderManagementOperations:
             if  old_provider_enrolment_details is None:
                 prob = ProblemDetails(title="Not Found", status=404, detail="Not Exist Provider Enrolment Details",
                                     cause="Not found registrations to send this api provider details")
-                return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype='application/json')
+                return Response(json.dumps(prob, cls=JSONEncoder), status=403, mimetype= self.mimetype)
 
             else:
 
@@ -100,8 +101,8 @@ class ProviderManagementOperations:
 
                 mycol.update_one(old_provider_enrolment_details, {"$set":api_provider_enrolment_details_patch})
 
-                return  Response(json_util.dumps(api_provider_enrolment_details_patch, cls=JSONEncoder), status=200, mimetype='application/json')
+                return  Response(json_util.dumps(api_provider_enrolment_details_patch, cls=JSONEncoder), status=200, mimetype= self.mimetype)
 
         except Exception as e:
-            print("An exception occurred ::", e, file=sys.stderr)
-            return False
+            exception = "An exception occurred in patch provider::", e
+            return Response(json.dumps(exception, default=str, cls=JSONEncoder), status=500, mimetype=self.mimetype)
