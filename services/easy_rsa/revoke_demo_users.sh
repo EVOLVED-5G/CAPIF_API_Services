5f5b4ac00fc6ab82b16b2e47a63a1cfd9bf961c4
Jorge Moratinos Salcines <jorge.moratinossalcines@telefonica.com>
2022-10-03 12:41:09 +0200

    Code Review, remove commented code at bash script revoke users

--- services/easy_rsa/revoke_demo_users.sh
+++ services/easy_rsa/revoke_demo_users.sh
@@ -3,13 +3,3 @@ DUMMY=dummy
 CUSTOMER_EXPOSER=customexposer
 ./revoke_user.sh $DUMMY
 ./revoke_user.sh $CUSTOMER_EXPOSER
-
-# /root/EasyRSA-3.0.4/easyrsa --batch revoke $DUMMY
-# rm -rf /root/$DUMMY.csr
-# rm -rf /root/pki/issued/$DUMMY.crt
-# rm -rf /root/pki/reqs/$DUMMY.req
-
-# /root/EasyRSA-3.0.4/easyrsa --batch revoke $CUSTOMER_EXPOSER
-# rm -rf /root/$CUSTOMER_EXPOSER.csr
-# rm -rf /root/pki/issued/$CUSTOMER_EXPOSER.crt
-# rm -rf /root/pki/reqs/$CUSTOMER_EXPOSER.req
