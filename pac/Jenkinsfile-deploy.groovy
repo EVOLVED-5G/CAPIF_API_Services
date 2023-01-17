607cd190a9f58ffd25ed16e001f72388d0f89450
Cesuuur <cesar.redondourdiales@telefonica.com>
2023-01-17 12:39:42 +0100

    chore: remove unused references

--- pac/Jenkinsfile-deploy.groovy
+++ pac/Jenkinsfile-deploy.groovy
@@ -37,7 +37,6 @@ pipeline {
     }
     parameters {
         string(name: 'BRANCH_NAME', defaultValue: 'develop', description: 'Deployment git branch name')
-        string(name: 'AWS_DEFAULT_REGION', defaultValue: 'eu-central-1', description: 'AWS region')
         string(name: 'OPENSHIFT_URL', defaultValue: 'https://api.ocp-epg.hi.inet:6443', description: 'openshift url')
         string(name: 'NGINX_HOSTNAME', defaultValue: 'openshift.evolved-5g.eu', description: 'Nginx eposition route in OpenShift')
         choice(name: 'DEPLOYMENT', choices: ['openshift', 'kubernetes-athens', 'kubernetes-uma'])
@@ -46,7 +45,6 @@ pipeline {
         // This is to work around a jenkins bug on the first build of a multi-branch job
         // https://issues.jenkins-ci.org/browse/JENKINS-40574 - it is marked resolved but the last comment says it doesn't work for declaritive pipelines
         BRANCH_NAME = "${params.BRANCH_NAME}"
-        AWS_DEFAULT_REGION = "${params.AWS_DEFAULT_REGION}"
         OPENSHIFT_URL = "${params.OPENSHIFT_URL}"
         DEPLOYMENT = "${params.DEPLOYMENT}"
         CONFIG_PATH = getPath("${params.DEPLOYMENT}")
@@ -70,17 +68,15 @@ pipeline {
             }
             steps {
                 withKubeConfig([credentialsId: 'kubeconfig-os-evol5-capif']) {
-                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: '328ab84a-aefc-41c1-aca2-1dfae5b150d2', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-                        dir ("${env.WORKSPACE}/iac/terraform/openshift4") {
-                            sh '''
-                                terraform init  -reconfigure                                             \
-                                    -backend-config="bucket=evolved5g-openshift-terraform-states"        \
-                                    -backend-config="key=capif"
-                                terraform validate
-                                terraform plan -var CAPIF_HOSTNAME=${NGINX_HOSTNAME}  -out deployment.tfplan
-                                terraform apply --auto-approve -lock-timeout=15m deployment.tfplan
-                            '''
-                        }
+                    dir ("${env.WORKSPACE}/iac/terraform/openshift4") {
+                        sh '''
+                            terraform init  -reconfigure                                             \
+                                -backend-config="bucket=evolved5g-openshift-terraform-states"        \
+                                -backend-config="key=capif"
+                            terraform validate
+                            terraform plan -var CAPIF_HOSTNAME=${NGINX_HOSTNAME}  -out deployment.tfplan
+                            terraform apply --auto-approve -lock-timeout=15m deployment.tfplan
+                        '''
                     }
                 }
             }
