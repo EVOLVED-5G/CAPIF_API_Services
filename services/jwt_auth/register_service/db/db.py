f430977838f7d86ae0d42ed6236b2a922a1fe1ce
nacho <hjoseigancio@gmail.com>
2023-03-20 12:13:42 +0100

    connect different db to test service

--- services/jwt_auth/register_service/db/db.py
+++ services/jwt_auth/register_service/db/db.py
@@ -6,11 +6,13 @@ class MongoDatabse():
 
     def __init__(self):
         self.config = Config().getConfig()
-        self.db = self.__connect()
+        self.db, self.db_capif = self.__connect()
         self.capif_users = self.config['mongo']['col']
 
 
     def get_col_by_name(self, name):
+        if name != "user":
+            return self.db_capif[name]
         return self.db[name]
 
     def __connect(self):
@@ -20,7 +22,8 @@ class MongoDatabse():
         try:
             client.admin.command("ping")
             mydb = client[self.config['mongo']['db']]
-            return mydb
+            mydb_capif =client[self.config['mongo']['db_capif']]
+            return mydb, mydb_capif
         except Exception as e:
             print("An exception occurred ::", e)
             return None
