fa3e0da7c0b79a95b6ebbcb60874fecfee979631
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2023-04-21 13:50:26 +0200

    redis-cli

--- services/TS29222_CAPIF_API_Provider_Management_API/prepare_provider.sh
+++ services/TS29222_CAPIF_API_Provider_Management_API/prepare_provider.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sleep 20
-redis-cli -h redis -p 6379 -n 1 GET  server_pub_key > /usr/src/app/api_provider_management/pubkey.pem
+OUTPUT=$(redis-cli -h redis -p 6379 -n 1 GET  server_pub_key)
+echo "$OUTPUT"
+
+if [ -z "$OUTPUT" ]; then
+    echo "OUTPUT empty"
+    exit 1;
+else
+    echo "OUTPUT: $OUTPUT"
+    redis-cli -h redis -p 6379 -n 1 GET  server_pub_key > /usr/src/app/api_provider_management/pubkey.pem
+fi
 
 cd /usr/src/app/
 python3 -m api_provider_management
--- services/jwt_auth/register_prepare.sh
+++ services/jwt_auth/register_prepare.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sleep 10
-redis-cli -h redis -p 6379 -n 1 GET  server_priv_key > /usr/src/app/register_service/server.key
+OUTPUT=$(redis-cli -h redis -p 6379 -n 1 GET  server_priv_key)
+echo "$OUTPUT"
+
+if [ -z "$OUTPUT" ]; then
+    echo "OUTPUT empty"
+    exit 1;
+else
+    echo "OUTPUT: $OUTPUT"
+    redis-cli -h redis -p 6379 -n 1 GET  server_priv_key > /usr/src/app/register_service/server.key
+fi
 
 cd /usr/src/app/
 python3 -m register_service
